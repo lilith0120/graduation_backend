4b64c921d3e9e49bd9db7a440a109944d4e38ad4
lilith <1131155106@qq.com>
2022-03-17 12:35:00 +0800

    feat:增加跨域

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import * as Koa from 'koa';
+import * as cors from 'koa2-cors';
 import * as parser from 'koa-bodyparser';
 import * as body from 'koa-body';
 import InitManager from './core/init';
 import catchError from './middlewares/exception';
 
 const app = new Koa();
+app.use(cors());
 app.use(catchError);
 app.use(body({
     multipart: true,
